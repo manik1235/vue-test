33076219e0f9556d6143b0c177999eb4af754a32
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2019-05-12 22:34:11 -0400

    Add styles, develop components
    This commit adds styles to the index, and the beginnings of the bucket
    component.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-alert('asdf');
+function bootApp() {
+
+  // Define a component
+  // Q: What can it be?
+  // A: A canvas, perhaps
+
+  // The bucket component
+  // A bucket contains
+  //   A source
+  //   A drain
+  //   An inflow
+  //   An outflow
+  Vue.component('bucket-component', {
+    template: `
+    <div class="bucket">
+      bucket
+      <div class="bucket-input">
+        bucket-input
+        <br>Source: <bucket-input></bucket-input>
+        <br>Drain: <bucket-input></bucket-input>
+      </div>
+    </div>`
+  })
+
+  Vue.component('bucket-input', {
+    template: '<input>{{ bucketValue }}</input>'
+  })
+
+  Vue.component('button-counter', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+  })
+
+  new Vue({ el: '#components-demo' })
+
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Welcome to the Vue in You'
+    }
+  })
+}
+
+bootApp();
